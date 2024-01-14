56a4511ac578530e0937a001646932111d72ad71
asadbek <asadbekoffical1202@gmail.com>
2024-01-14 14:30:51 +0500

    Updated: url

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ urlpatterns = [
     path("questions/", QuestionList.as_view(), name="question-list"),
     path("regstration/", RegisterView.as_view(), name="register"),
     path("login/", LoginWithTokenView.as_view(), name="login"),
-    path("test-analyz/", TestAnalytics.as_view(), name="test-analytics")
+    # path("test-analyz/", TestAnalytics.as_view(), name="test-analytics")
 
 
 
